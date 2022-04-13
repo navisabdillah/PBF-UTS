ffb6c7499171e00128b5e16d6355614d1fddfe20
navisabdillah <mavisabdi13@gmail.com>
2022-04-13 20:58:28 +0700

    HOC Click Beli

---
+++ src/Component/ClickBeli.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class ClickBeli extends Component {
+    render() {
+        <a class="nav-link" href="/list_item">Menu Makakanan</a>
+        return  <button>Click Beli </button>      
+        
+
+    }
+}
+
+export default ClickBeli
